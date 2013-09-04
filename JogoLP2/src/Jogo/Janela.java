b4516bdfc5181328a0df67ed5a7271e760b5d89a
sidneyspe@gmail.com <sidneyspe@gmail.com@7e6ab3ab-2bad-5275-4b6c-171f90792e1a>
2013-09-04 01:47:57 +0000

    Adicionado imagens dos botões, tela principal e créditos Mudança dos icones dos botões

--- JogoLP2/src/Jogo/Janela.java
+++ JogoLP2/src/Jogo/Janela.java
@@ -2,10 +2,19 @@ package Jogo;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.util.List;
 
 import javax.swing.AbstractButton;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JTable;
@@ -15,16 +24,26 @@ import javax.swing.table.DefaultTableModel;
 public class Janela extends JFrame{
 	JPanel painel = new JPanel();
 	private JTable table;
+	private Image fundo;
+	JButton b1;
+	JLabel l1;
+	
 	public Janela() {
 		iniciar();
+					
 		getContentPane().setLayout(null);
 		painel.setBounds(0, 0, 1, 561);
 		getContentPane().add(painel);
-		
 		painel.setLayout(null);
+				
 		
 		JButton btnIniciar = new JButton("INICIAR");
-		btnIniciar.setBounds(51, 105, 250, 100);
+		btnIniciar.setBorderPainted(false);
+		btnIniciar.setContentAreaFilled(false); 
+		Icon iconeIniciar = new ImageIcon("res//btnIniciar.png");
+		btnIniciar.setIcon( iconeIniciar );
+		btnIniciar.setFocusPainted(false); 
+		btnIniciar.setBounds(101, 300, 128, 35);
 		getContentPane().add(btnIniciar);
 		btnIniciar.addActionListener(new ActionListener() {
 			
@@ -37,17 +56,27 @@ public class Janela extends JFrame{
 		});
 		
 		JButton btncredito = new JButton("CRÉDITO");
+		btncredito.setBounds(101, 360, 125, 50);
+		btncredito.setBorderPainted(false);
+		btncredito.setContentAreaFilled(false); 
+		Icon iconeCredito = new ImageIcon("res//btnCredito.png");
+		btncredito.setIcon( iconeCredito );
+		getContentPane().add(btncredito);
 		btncredito.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				new Janela3();
 				setVisible(false);
 			}
 		});
-		btncredito.setBounds(51, 233, 250, 100);
-		getContentPane().add(btncredito);
+		
 		
 		JButton btnSair = new JButton("SAIR");
-		btnSair.setBounds(51, 361, 250, 100);
+		btnSair.setBounds(101, 420, 125, 50);
+		btnSair.setBorderPainted(false);
+		btnSair.setContentAreaFilled(false); 
+		Icon iconeSair = new ImageIcon("res//btnSair.png");
+		btnSair.setIcon( iconeSair );
+		
 		getContentPane().add(btnSair);
 		btnSair.addActionListener(new ActionListener() {
 			
@@ -58,13 +87,21 @@ public class Janela extends JFrame{
 		});		
 	}
 
-
+	
+	
 	private void iniciar() {
 		setTitle("MENU PRINCIPAL");
 		setSize(800, 600);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setContentPane(new JLabel(new ImageIcon("res//inicial.jpg")));
+		setSize(801, 601);
+		setSize(800, 600);
+		
+		
+		
 	}
+	
 	
 	public static void main(String[] args) {
 		new Janela();
--- JogoLP2/src/Jogo/Janela3.java
+++ JogoLP2/src/Jogo/Janela3.java
@@ -4,32 +4,50 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Janela3 extends JFrame{
 
 	public Janela3(){
 		setTitle("CRÉDITO");
-		
 		setSize(800, 600);
+		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		JPanel panel = new JPanel();
+		setContentPane(new JLabel(new ImageIcon("res//creditos.jpg")));
+		setSize(801, 601);
+		setSize(800, 600);	
+		
+		
 		getContentPane().add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
 		
 		JButton btnSair = new JButton("RETORNAR");
+		panel.add(btnSair);
+		btnSair.setBounds(101, 420, 125, 50);
+		btnSair.setBorderPainted(false);
+		btnSair.setContentAreaFilled(false); 
+		btnSair.setFocusPainted(false); 
+
+		Icon iconeSair = new ImageIcon("res//btnRetornar.png");
+		btnSair.setIcon( iconeSair );
+		getContentPane().add(btnSair);
+
 		btnSair.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				new Janela();
 				setVisible(false);
 			}
 		});
-		btnSair.setBounds(51, 361, 250, 100);
-		panel.add(btnSair);
-		setVisible(true);
+		
+		
+		
 	}
 	
 
